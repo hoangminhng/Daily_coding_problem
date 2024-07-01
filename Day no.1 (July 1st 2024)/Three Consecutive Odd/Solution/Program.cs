584648295ae7120f672ad678fa1cd6ef2eecf7d5
HoangMinhNg <ngminh3004@gmail.com>
2024-07-01 10:49:08 +0700

    day no.1

---
+++ Day no.1 (July 1st 2024)/Three Consecutive Odd/Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public bool ThreeConsecutiveOdds(int[] arr)
+    {
+        int count = 0;
+        for (int x = 0; x < arr.Length; x++)
+        {
+            if (arr[x] % 2 != 0)
+            {
+                count++;
+                if (count == 3)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
+
+sealed partial class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        int[] arr1 = { 2, 6, 4, 1 };
+        int[] arr2 = { 1, 2, 34, 3, 4, 5, 7, 23, 12 };
+        System.Console.WriteLine(solution.ThreeConsecutiveOdds(arr1));
+        System.Console.WriteLine(solution.ThreeConsecutiveOdds(arr2));
+    }
+}
